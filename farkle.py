4f958eab2fb3481e0e6501dffc8e75e1cf4400de
Aaron Freed <aaron.freed@mckesson.com>
2023-12-20 17:02:53 -0500

    Functioning simulator
    Basic simulation of Farkle + monte carlo capability. Currently using a greedy algorithm for score selection.
    
    Simulator will try to score all points off a roll that it can rather than trying to maximize attempts at rolling with more dice. So if you roll two 1s, one 5, two 4s, and one 6 the simulator scores the 1s and 5s for 250 points rather than scoring 100 and rerolling the five remaining dice.

--- farkle.py
+++ farkle.py
@@ -1,3 +1,4 @@
+#%% [Import Section]
 # =============================================================================
 # Import Section Start
 # =============================================================================
@@ -12,129 +13,171 @@ import numpy as np
 #%% [Function Definitions]
 
 def die_roll(dice = 1):
-    result = pd.Series(np.random.randint(1,6,6))
-    score = score_check(result)
-    return score, result
+    result = pd.Series(np.random.randint(1,7,dice))
+    score, dice_avail = score_check(result, dice)
+    return score, result.values, dice_avail
 
 def farkle_check(score):
     if score > 0:
         return 1
     else: return 0
-
-def check_5(value_counts, score):
+    
+def check_6(value_counts, score, dice_avail):
+    if value_counts.iloc[0] == 6:
+        score = 3000
+        return score, 6
+    elif value_counts.iloc[0] == 3 and value_counts.iloc[1] == 3:
+        score = 2500
+        return score, 6
+    elif value_counts.iloc[0] == 4 and value_counts.iloc[1] == 2:
+        score = 1500
+        return score, 6
+    elif value_counts.iloc[0] == 2 and value_counts.iloc[1] == 2 and value_counts.iloc[2] == 2:
+        score = 1500
+        return score, 6
+    return score, dice_avail
+    
+def check_5(value_counts, score, dice_avail):
     if value_counts.iloc[0] == 5:
         score += 2000
+        dice_avail -= 5
         if 1 in value_counts.index[1:]:
             score += 100*value_counts[1]
+            dice_avail -= value_counts[1]
         if 5 in value_counts.index[1:]:
             score += 50*value_counts[5]
-    return score    
+            dice_avail -= value_counts[5]
+    return score, dice_avail
 
-def check_4(value_counts, score):
+def check_4(value_counts, score, dice_avail):
     if value_counts.iloc[0] == 4:
         score += 1000
+        dice_avail -= 4
         if 1 in value_counts.index[1:]:
             score += 100*value_counts[1]
+            dice_avail -= value_counts[1]
         if 5 in value_counts.index[1:]:
             score += 50*value_counts[5]
-    return score
+            dice_avail -= value_counts[5]
+    return score, dice_avail
 
-    5 in value_counts[5]
-
-def check_3(value_counts, score):
+def check_3(value_counts, score, dice_avail):
     if value_counts.iloc[0] == 3:
         score += 100*value_counts.index[0]
+        dice_avail -= 3
         if 1 in value_counts.index[1:]:
             score += 100*value_counts[1]
+            dice_avail -= value_counts[1]
         if 5 in value_counts.index[1:]:
             score += 50*value_counts[5]
-    return score
+    return score, dice_avail
+
+def check_21(value_counts, score, dice_avail):
+    if 1 in value_counts.index:
+        score += 100*value_counts[1]
+        dice_avail -= value_counts[1]
+    if 5 in value_counts.index:
+        score += 50*value_counts[5]
+        dice_avail -= value_counts[5]
+    return score, dice_avail
     
-def score_check(result):
+def score_check(result, dice):
     score = 0
+    dice_avail = dice
     value_counts = result.value_counts()    
+    # 6 dice score combos
+    if sorted(result) == [1,2,3,4,5,6]:
+        return 1500, 6
     if len(result) == 6:
-        if value_counts.iloc[0] == 6:
-            score = 3000
-            return score
-        elif value_counts.iloc[0] == 3 and value_counts.iloc[1] == 3:
-            score = 2500
-            return score
-        elif value_counts.iloc[0] == 4 and value_counts.iloc[1] == 2:
-            score = 1500
-            return score
-        elif value_counts.iloc[0] == 2 and value_counts.iloc[1] == 2 and value_counts.iloc[2] == 2:
-            score = 1500
-            return score
-        elif sorted(result) == [1,2,3,4,5,6]:
-            score = 1500
-            return score
-        score = check_5(value_counts, score)
+        score, dice_avail = check_6(value_counts, score, dice_avail)
         if score > 0:
-            return score
-        score += check_4(value_counts, score)
+            return score, dice_avail
+    # 5 dice score combos
+        score, dice_avail = check_5(value_counts, score, dice_avail)
         if score > 0:
-            return score
-        score += check_3(value_counts, score)
+            return score, dice_avail  
+    # 4 dice score combos    
+        score, dice_avail = check_4(value_counts, score, dice_avail)
         if score > 0:
-            return score
-        if 1 in value_counts.index[1:]:
-            score += 100*value_counts[1]
-        if 5 in value_counts.index[1:]:
-            score += 50*value_counts[5]
+            return score, dice_avail
+    # 3 dice score combos
+        score, dice_avail = check_3(value_counts, score, dice_avail)
+        if score > 0:
+            return score, dice_avail 
+   # 2 dice or 1 die score combos
+        score, dice_avail = check_21(value_counts, score, dice_avail)
+        return score, dice_avail
     if len(result) == 5:
-        score += check_5(value_counts, score)
-        if score > 0:
-            return score
-        score += check_4(value_counts, score)
-        if score > 0:
-            return score
-        score += check_3(value_counts, score)
-        if score > 0:
-            return score
-        if 1 in value_counts.index[1:]:
-            score += 100*value_counts[1]
-        if 5 in value_counts.index[1:]:
-            score += 50*value_counts[5]
+   # 5 dice score combos
+         score, dice_avail = check_5(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail  
+   # 4 dice score combos    
+         score, dice_avail = check_4(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail
+   # 3 dice score combos
+         score, dice_avail = check_3(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail 
+   # 2 dice or 1 die score combos
+         score, dice_avail = check_21(value_counts, score, dice_avail)
+         return score, dice_avail
     if len(result) == 4:
-        score += check_4(value_counts, score)
-        if score > 0:
-            return score
-        score += check_3(value_counts, score)
-        if score > 0:
-            return score
-        if 1 in value_counts.index[1:]:
-            score += 100*value_counts[1]
-        if 5 in value_counts.index[1:]:
-            score += 50*value_counts[5]
+   # 4 dice score combos    
+         score, dice_avail = check_4(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail
+   # 3 dice score combos
+         score, dice_avail = check_3(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail 
+   # 2 dice or 1 die score combos
+         score, dice_avail = check_21(value_counts, score, dice_avail)
+         return score, dice_avail
     if len(result) == 3:
-        score += check_3(value_counts, score)
-        if score > 0:
-            return score
-        if 1 in value_counts.index[1:]:
-            score += 100*value_counts[1]
-        if 5 in value_counts.index[1:]:
-            score += 50*value_counts[5]
+   # 3 dice score combos
+         score, dice_avail = check_3(value_counts, score, dice_avail)
+         if score > 0:
+             return score, dice_avail 
+   # 2 dice or 1 die score combos
+         score, dice_avail = check_21(value_counts, score, dice_avail)
+         return score, dice_avail
     else:
-        if 1 in value_counts.index[1:]:
-            score += 100*value_counts[1]
-        if 5 in value_counts.index[1:]:
-            score += 50*value_counts[5]
-    return score
+   # 2 dice or 1 die score combos
+         score, dice_avail = check_21(value_counts, score, dice_avail)
+         return score, dice_avail
 
-def play_a_round(roll_dice, used_dice, score):
-    score_hold, result = die_roll(roll_dice)
-    
+def play_a_round(score, roll_dice):
+    results = pd.DataFrame([die_roll(roll_dice)], columns = ['Score', 'Result', 'Dice Avail'] )
+    dice_avail = results['Dice Avail'].values[0]
+    while roll_dice > dice_avail and results['Score'].cumsum().tail(1).values[0]:
+        roll_dice = dice_avail
+        if roll_dice == 0:
+            roll_dice = 6
+        results = pd.concat([results, pd.DataFrame([die_roll(roll_dice)], columns = ['Score', 'Result', 'Dice Avail'])])
+        dice_avail = results.tail(1)['Dice Avail'].values[0]
+    results['Cum Score'] = results['Score'].cumsum()
+    return results['Cum Score'].tail(1).values[0]
+
+def score_analyzer(score, roll_dice):
+    mc = pd.DataFrame([play_a_round(score,roll_dice) for i in range(10000)], columns = ['Score'], dtype = object )
+    farkle_chance = mc['Score'].value_counts()[0]/10000
+    score_chance = 1-farkle_chance
+    poss_score = mc['Score'].mean()*score_chance+score
+    mc['Poss Score'] = poss_score
+    mc['Score %'] = score_chance*100
+    mc['Farkle %'] = farkle_chance*100
+    return mc
 #%%
-score = input("Enter current score:")
+score = int(input("Enter current score:"))
 roll_dice = int(input("Enter dice available to roll:"))
-used_dice = 6-roll_dice
 
-die_roll(roll_dice)
-mc = pd.DataFrame([die_roll(roll_dice) for i in range(10000)], columns = ['Score', 'Result'] )
+#results = pd.DataFrame([play_a_round(score, roll_dice)], columns = ['Poss Score', 'Score %', 'Farkle %'])
+results = score_analyzer(score, roll_dice)
+results['Current Score'] = score
+results = results[['Current Score', 'Poss Score', 'Score %', 'Farkle %']]
+print(results)
 
-mc['Score'].mean()
-
-farkle_chance = mc['Score'].value_counts()[0]/10000
-
-mc['Score'].plot.hist()
+farkle_risk = {6:.023, 5:.079, 4:.1602, 3:.2808, 2:.4444, 1:.6667}
+score_risk = {6:650, 5:400, 4:300, 3:200, 2: 150, 1:50}
